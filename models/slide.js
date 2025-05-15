b4b0a934b5ea9ce5d12fda92dfbb704abefe8d10
amer200 <amermostafa@gmail.com>
2025-05-15 17:44:59 +0300

    commit

--- models/slide.js
+++ models/slide.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const sliderImageSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
-  altText: { type: String, default: '' }
+    imageUrl: { type: String, required: true },
+    altText: { type: String, default: '' }
 });
 
 module.exports = mongoose.model('SliderImage', sliderImageSchema);
